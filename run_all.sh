97180ed433d15b7583588ee997ca68bdc73dda38
InfinitiWarrior <79752294+InfinitiWarrior@users.noreply.github.com>
2024-09-09 11:14:37 +0200

    fixed logical mistake

--- run_all.sh
+++ run_all.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Set executable permissions for all scripts
-chmod +x dependencies.sh
 chmod +x user_setup.sh
+chmod +x dependencies.sh
 chmod +x maintenance.sh
 chmod +x folder_setup.sh
 chmod +x logging_and_backup.sh
 
 # Run all setup and configuration scripts
-./dependencies.sh
 ./user_setup.sh
+./dependencies.sh
 ./maintenance.sh
 ./folder_setup.sh
 ./logging_and_backup.sh
